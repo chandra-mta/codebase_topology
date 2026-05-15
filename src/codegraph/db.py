9c49fb154806b359047378ae401f4a0814cc7483
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2026-05-15 14:30:51 -0400

    database naming scheme variability.

--- src/codegraph/db.py
+++ src/codegraph/db.py
@@ -3,13 +3,14 @@ from pathlib import Path
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DB_DIR = os.getenv("DB_DIR")
+TOPOLOGY_DB_NAME = os.getenv("TOPOLOGY_DB_NAME", "mta_codebase.db")
+TOPOLOGY_DB_DIR = os.getenv("TOPOLOGY_DB_DIR")
 _echo = bool(os.getenv('ECHO', False))
-if DB_DIR is None:
+if TOPOLOGY_DB_DIR is None:
     #: Relative Path
-    DB_URL = "sqlite:///mta_codebase.db"
+    DB_URL = f"sqlite:///{TOPOLOGY_DB_NAME}"
 else:
-    DB_URL = f"sqlite:///{Path(DB_DIR, "mta_codebase.db")}"
+    DB_URL = f"sqlite:///{Path(TOPOLOGY_DB_DIR, TOPOLOGY_DB_NAME)}"
 
 engine = create_engine(DB_URL, echo=_echo)
 SessionLocal = sessionmaker(bind=engine)
